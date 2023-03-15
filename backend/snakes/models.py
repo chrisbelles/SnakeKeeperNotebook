e99dc776ac44bea2f2f19717609a488bc403cfe2
chrisbelles <chris.belles1@gmail.com>
2023-03-14 23:59:22 -0500

    Added the automatic calculation for next_feeding from last_fed plus the feeding interval

--- backend/snakes/models.py
+++ backend/snakes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from authentication.models import User
 from django.utils import timezone
+from datetime import datetime, timedelta
+
 
 class Snake(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -11,25 +13,16 @@ class Snake(models.Model):
     genetics = models.CharField(max_length=100)
 
 
-
 class Feeding(models.Model):
     snake = models.ForeignKey(Snake, on_delete=models.CASCADE)
     last_fed = models.DateField()
     feeding_interval = models.IntegerField()
-    next_feeding = models.DateField()
+    next_feeding = models.DateField(blank=True, null=True)
 
-    # def calculate_next_feeding(self):
-    #     if self.last_fed and self.feeding_interval:
-    #         days = timezone.now() - self.last_fed
-    #         days_since_last_fed = days.days
-    #         days_until_next_feeding = self.feeding_interval - days_since_last_fed
-    #         if days_until_next_feeding < 0:
-    #             self.next_feeding = timezone.now()
-    #         else:
-    #             self.next_feeding = timezone.now() + timezone.timedelta(days=days_until_next_feeding)
-    #     else:
-    #         self.next_feeding = None
+    def save(self, *args, **kwargs):
+        last_fed_datetime = timezone.make_aware(datetime.combine(self.last_fed, datetime.min.time()))
+        days = (timezone.now() - last_fed_datetime).days
+        next_feeding_datetime = last_fed_datetime + timedelta(days=self.feeding_interval)
+        self.next_feeding = next_feeding_datetime.date()
+        super().save(*args, **kwargs)
 
-    # def save(self, *args, **kwargs):
-    #     self.calculate_next_feeding()
-    #     super().save(*args, **kwargs)
