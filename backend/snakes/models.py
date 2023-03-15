530616400f778886f6dc93c7e3138cd8884c6a38
chrisbelles <chris.belles1@gmail.com>
2023-03-14 23:40:15 -0500

    Created feeding model and serializer for the first step of relational database.

--- backend/snakes/models.py
+++ backend/snakes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from authentication.models import User
+from django.utils import timezone
 
 class Snake(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -8,3 +9,27 @@ class Snake(models.Model):
     age = models.IntegerField()
     weight = models.IntegerField()
     genetics = models.CharField(max_length=100)
+
+
+
+class Feeding(models.Model):
+    snake = models.ForeignKey(Snake, on_delete=models.CASCADE)
+    last_fed = models.DateField()
+    feeding_interval = models.IntegerField()
+    next_feeding = models.DateField()
+
+    # def calculate_next_feeding(self):
+    #     if self.last_fed and self.feeding_interval:
+    #         days = timezone.now() - self.last_fed
+    #         days_since_last_fed = days.days
+    #         days_until_next_feeding = self.feeding_interval - days_since_last_fed
+    #         if days_until_next_feeding < 0:
+    #             self.next_feeding = timezone.now()
+    #         else:
+    #             self.next_feeding = timezone.now() + timezone.timedelta(days=days_until_next_feeding)
+    #     else:
+    #         self.next_feeding = None
+
+    # def save(self, *args, **kwargs):
+    #     self.calculate_next_feeding()
+    #     super().save(*args, **kwargs)
--- backend/snakes/serializers.py
+++ backend/snakes/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Snake
+from .models import Snake, Feeding
 
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
@@ -8,4 +8,10 @@ class SnakeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snake
         fields = ['id', 'name', 'gender', 'age', 'weight', 'genetics']
+        depth = 1
+
+class FeedingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feeding
+        fields = '__all__'
         depth = 1
