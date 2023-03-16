d8e7dab1d2192b1f75849e1c56a95ac6c5797387
chrisbelles <chris.belles1@gmail.com>
2023-03-15 22:29:22 -0500

    Fixed bugs exposed when adding markers for breeding pairs + added naming to admin site to be able to keep everything understandable on the backend

--- backend/snakes/admin.py
+++ backend/snakes/admin.py
@@ -1,8 +1,48 @@
 from django.contrib import admin
 from .models import Snake, Feeding, Cleaning, BreedingPair
+from datetime import datetime,timezone
 
-# Register your models here.
-admin.site.register(Snake)
-admin.site.register(Feeding)
-admin.site.register(Cleaning)
-admin.site.register(BreedingPair)
+class FeedingAdmin(admin.ModelAdmin):
+    list_display = ('snake', 'last_fed', 'feeding_interval', 'next_feeding', 'mark_feeding_completed')
+
+    def mark_feeding_completed(self, obj):
+        if obj.next_feeding == datetime.now().date():
+            return True
+        else:
+            return False
+
+    mark_feeding_completed.boolean = True
+
+class CleaningAdmin(admin.ModelAdmin):
+    list_display = ('snake', 'last_cleaned', 'cleaning_interval', 'next_cleaning', 'mark_cleaning_completed')
+
+    def mark_cleaning_completed(self, obj):
+        if obj.next_cleaning == datetime.now().date():
+            return True
+        else:
+            return False
+
+    mark_cleaning_completed.boolean = True
+
+class SnakeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'gender', 'age', 'weight', 'genetics', 'paired')
+
+class BreedingPairAdmin(admin.ModelAdmin):
+    list_display = ('male', 'female', 'is_paired', 'start_date', 'end_date')
+
+    def is_paired(self, obj):
+        return obj.male.paired and obj.female.paired
+
+    is_paired.boolean = True
+    is_paired.short_description = 'Paired'
+
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
+
+
+
+admin.site.register(Snake, SnakeAdmin)
+admin.site.register(Feeding, FeedingAdmin)
+admin.site.register(Cleaning, CleaningAdmin)
+admin.site.register(BreedingPair, BreedingPairAdmin)
--- backend/snakes/models.py
+++ backend/snakes/models.py
@@ -13,6 +13,30 @@ class Snake(models.Model):
     genetics = models.CharField(max_length=100)
     paired = models.BooleanField(default=False)
 
+    @property
+    def breeding_pair(self):
+        pairs = BreedingPair.objects.filter(models.Q(male=self) | models.Q(female=self))
+        if pairs.exists():
+            return pairs[0]
+        else:
+            return None
+
+    def needs_feeding(self):
+        try:
+            last_feeding = self.feeding_set.latest('last_fed')
+            return last_feeding.next_feeding == datetime.now().date()
+        except Feeding.DoesNotExist:
+            return False
+
+    def needs_cleaning(self):
+        try:
+            last_cleaning = self.cleaning_set.latest('last_cleaned')
+            return last_cleaning.next_cleaning == datetime.now().date()
+        except Cleaning.DoesNotExist:
+            return False
+        
+    def __str__(self):
+            return self.name
 
 class Feeding(models.Model):
     snake = models.ForeignKey(Snake, on_delete=models.CASCADE)
@@ -26,6 +50,9 @@ class Feeding(models.Model):
         next_feeding_datetime = last_fed_datetime + timedelta(days=self.feeding_interval)
         self.next_feeding = next_feeding_datetime.date()
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.snake.name} - Last Fed: {self.last_fed.strftime("%m/%d/%Y")} - Next Feeding: {self.next_feeding.strftime("%m/%d/%Y")}'
 
 
 class Cleaning(models.Model):
@@ -43,8 +70,35 @@ class Cleaning(models.Model):
         self.calculate_next_cleaning()
         super().save(*args, **kwargs)
 
+    def __str__(self):
+        return f"{self.snake.name}'s cleaning schedule"
+
 class BreedingPair(models.Model):
     male = models.ForeignKey(Snake, on_delete=models.CASCADE, related_name='breeding_pairs_as_male')
     female = models.ForeignKey(Snake, on_delete=models.CASCADE, related_name='breeding_pairs_as_female')
     start_date = models.DateField()
     end_date = models.DateField(null=True, blank=True)
+
+    def is_paired(self):
+        return self.male.paired and self.female.paired
+    is_paired.boolean = True
+    is_paired.short_description = 'Paired'
+
+    def save(self, *args, **kwargs):
+        # set paired field to True for both male and female snakes
+        self.male.paired = True
+        self.female.paired = True
+        self.male.save()
+        self.female.save()
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        # unset paired field for both male and female snakes
+        self.male.paired = False
+        self.female.paired = False
+        self.male.save()
+        self.female.save()
+        super().delete(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.male.name} and {self.female.name} ({self.start_date} - {self.end_date})"
