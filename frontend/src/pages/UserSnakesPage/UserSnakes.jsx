83f6210bce7dfce4621558fa4c973be39c2d5e9c
chrisbelles <chris.belles1@gmail.com>
2023-03-16 13:01:02 -0500

    started altering code to apply to my project + created UserSnakesPage folder with UserSnakes module.

---
+++ frontend/src/pages/UserSnakesPage/UserSnakes.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
+
+const UserSnakes = () => {
+  const [user, token] = useAuth();
+  const [snakes, setSnakes] = useState([]);
+
+  useEffect(() => {
+    const fetchSnakes = async () => {
+      try {
+        let response = await axios.get("http://127.0.0.1:8000/api/snakes/", {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        });
+        setSnakes(response.data);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    };
+    fetchSnakes();
+  }, [token]);
+
+  return (
+    <div className="container">
+      <h1>Your Snakes</h1>
+      <Link to="/">Go back to home</Link>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Age</th>
+            <th>Weight</th>
+            <th>Genetics</th>
+            <th>Paired</th>
+          </tr>
+        </thead>
+        <tbody>
+          {snakes.map((snake) => (
+            <tr key={snake.id}>
+              <td>{snake.name}</td>
+              <td>{snake.gender}</td>
+              <td>{snake.age}</td>
+              <td>{snake.weight}</td>
+              <td>{snake.genetics}</td>
+              <td>{snake.paired ? "Yes" : "No"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserSnakes;
